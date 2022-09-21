7c024a9d422aed58f9e0cc5589b8bd2583cf413e
zacha <zachand9@gmail.com>
2022-09-21 05:09:46 -0500

    Add glass navbar, hover features

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,21 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div name="about" className='w-full h-screen bg-slate-900 text-gray-300'>
+        <div className='flex flex-col justify-center items-center w-full h-full'>
+            <div className='max-w-[1000px] w-full grid grid-cols-2 gap-8'>
+                <div className='sm:text-right pb-8 pl-4'>
+                    <p className='text-4xl font-bold inline border-b-4 border-pink-600'>About</p>
+                </div>
+                <div></div>
+                </div>
+                <div className='max-w-[1000px] w-full grid grid-cols-2 gap-8'>
+                    <div>
+                        <p>Hi. I'm Zachary, nice to meet you. Please take a look around.</p>
+                    </div>
+                </div>
+        </div>
+    </div>
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React , {useState} from 'react'
-import Logo from '../assets/logo.png'
+import Logo from '../assets/logo2.png'
 import {FaBars, FaTimes, FaGithub, FaLinkedin, FaFacebook, FaMailBulk, FaFile} from 'react-icons/fa'
 import {HiOutlineMail} from 'react-icons/hi'
 import {BsFillPersonLinesFill} from 'react-icons/bs'
@@ -9,19 +9,19 @@ const Navbar = () => {
     const handleClick = () => setNav(!nav);
 
   return (
-    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
-
+    <div className='fixed w-full h-[60px] flex justify-between items-center px-4 backdrop-blur-md border-b border-gray-800 text-gray-300'>
+        {/* <div className='fixed mt-14 w-full -ml-4 h-[1px] bg-gradient-to-r from-indigo-500 to-pink-500 background-animate'></div> */}
         <div>
-    <img src={Logo} alt="" style={{width: '50px'}}/>
+    <img src={Logo} alt="" style={{width: '60px'}}/>
         </div>
 
         {/* menu */}
-            <ul className='hidden md:flex'>
-                <li>Home</li>
-                <li>About</li>
-                <li>Skills</li>
-                <li>Work</li>
-                <li>Contact</li>
+            <ul className='hidden text-xs font-bold md:flex'>
+                <li className='hover:text-pink-500'>Home</li>
+                <li className='hover:text-pink-500'>About</li>
+                <li className='hover:text-pink-500'>Skills</li>
+                <li className='hover:text-pink-500'>Work</li>
+                <li className='hover:text-pink-500'>Contact</li>
             </ul>
 
         {/* Hamburger */}
@@ -29,7 +29,7 @@ const Navbar = () => {
             {!nav ? <FaBars /> : <FaTimes />}
         </div>
         {/* mobile menu */}
-        <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'}>
+        <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-gray-900 flex flex-col justify-center items-center'}>
             <li className='py-6 text-4xl'>Home</li>
             <li className='py-6 text-4xl'>About</li>
             <li className='py-6 text-4xl'>Skills</li>
@@ -37,11 +37,12 @@ const Navbar = () => {
             <li className='py-6 text-4xl'>Contact</li>
         </ul>
         {/* Social Icons */}
-        <div className='hidden xl:flex fixed flex-col top-[35%] justify-between left-0'>
+        <div className='hidden lg:flex fixed flex-col top-[100%] justify-between left-0'>
             <ul>
-                <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 hover:bg-gray-800/90 rounded-md bg-gray-800/40'>
+                <li className='w-[150px] h-[40px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 hover:bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-md bg-gray-800/40'>
+                    
                     <a className='flex justify-between items-center w-full text-gray-300' href="">
-                        LinkedIn <FaLinkedin size={30} />
+                        <p className='text-xs'>Let's connect!</p> <FaLinkedin size={20} />
                     </a>
                 </li>
                 <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 hover:bg-gray-800/90 rounded-md bg-gray-800/40'>
